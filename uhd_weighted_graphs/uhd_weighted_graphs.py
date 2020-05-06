a0fd273c1fb867830b8968effcfffc8e2db51fa0
maxwell-boozer <64792699+maxwell-boozer@users.noreply.github.com>
2020-05-06 16:28:46 -0500

    Update uhd_weighted_graphs.py
    Updated with comments

--- uhd_weighted_graphs/uhd_weighted_graphs.py
+++ uhd_weighted_graphs/uhd_weighted_graphs.py
@@ -1,3 +1,13 @@
+"""
+
+This is the main Python script to run to determine Prim's MST 
+for any graph G. 
+
+Follow the instructions in the Console to begin
+
+"""
+
+
 from algorithims.prims import *
 from functions.OS_Check import *
 
